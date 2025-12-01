e9349714c7dc3f1fbeb9b881db7dad904ba985e7
Gerald <geraldzinanyuca@gmail.com>
2025-12-01 00:21:28 -0500

    merge remoto v5

--- mobile/airguardnet-mobile/app/src/main/java/com/airguardnet/mobile/ui/auth/AuthScreen.kt
+++ mobile/airguardnet-mobile/app/src/main/java/com/airguardnet/mobile/ui/auth/AuthScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -23,7 +24,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
